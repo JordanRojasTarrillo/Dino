222effdcd7bc3c05f3c90dd40af14fb218b950db
JordanRojasTarrillo <jordanxyv@gmail.com>
2025-04-23 09:05:04 -0500

    chore: build actualizado para despliegue

---
+++ assets/js/36994c47.d0b10732.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmi_doc_site=self.webpackChunkmi_doc_site||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ assets/js/a7456010.23c49819.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmi_doc_site=self.webpackChunkmi_doc_site||[]).push([[1235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
